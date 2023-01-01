28b1cdb63a5b7ee5c812d1beb17d112eac6bb89a
Eliot Fagnon <efagnon2002@gmail.com>
2023-01-01 21:55:19 +0100

    colors and font values

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+      fontWeight: {
+          'base': "300",
+          'semi-bold': "600",
+          'bold': "800"
+      },
+    extend: {
+        colors: {
+            "dark-blue": "hsl(209, 23%, 22%)",
+            "very-dark-blue": "hsl(207, 26%, 17%)",
+            "very-dark-blue-text": "hsl(200, 15%, 8%)",
+            "dark-gray": "hsl(0, 0%, 52%)",
+            "light-gray": "hsl(0, 0%, 98%)",
+        },
+        fontFamily: {
+            "nunito": "Nunito"
+        }
+    },
   },
   plugins: [],
 }
