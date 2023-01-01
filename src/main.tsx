d00a61d58d204541f749d66e8dc0d80a6caf023c
Eliot Fagnon <efagnon2002@gmail.com>
2023-01-01 23:08:10 +0100

    add fontawesome moon icons to the top level

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faMoon} from "@fortawesome/free-solid-svg-icons";
+import {faMoon as farMoon} from "@fortawesome/free-regular-svg-icons";
+
+
+library.add(faMoon, farMoon);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
